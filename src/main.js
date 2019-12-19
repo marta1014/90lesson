617166a06fb1288a9a59138409b00c10553d1512
marta1014 <lovemadenghui@126.com>
2019-12-19 09:00:07 +0800

    清理目录 安装ele 引入静态资源 路由初配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'// 导入安装的依赖包
+import 'element-ui/lib/theme-chalk/index.css' // 引入ele样式文件
 Vue.config.productionTip = false
+Vue.use(ElementUI) // 全局注册ele
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../views/login'
+import Home from '../views/home'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
+  // {
+  // path: '/about',
+  // name: 'about',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
